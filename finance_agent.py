0911fef3119ad10055d7661504a20598bceeef3b
labib <alif16416@gmail.com>
2025-01-27 00:10:40 +0600

    finance agents working finely

--- finance_agent.py
+++ finance_agent.py
@@ -1,18 +1,23 @@
 from phi.agent import Agent
 from phi.model.groq import Groq 
+from phi.model.openai import OpenAIChat
 from phi.tools.yfinance import YFinanceTools
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-agent= Agent(
+
+finance_agent= Agent(
+    name="Finance Agent",
+    role="Get Financial Data",
     model=Groq(id="llama-3.3-70b-versatile"),
-    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)],
+    tools=[YFinanceTools(stock_price=True, stock_fundamentals=True,company_info=True)],
+    instructions=["Use tables to display data"],
     show_tool_calls=True,
-    markdown=True,
-    instructions=["Use tables to display data."]
+    markdown=True
      
 )
 
-agent.print_response("Figure Out The Live Stock Price  for TSLA and NVDIA stocks ")
+finance_agent.print_response("Get me the stock price of Apple Inc")
+
